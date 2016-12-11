63ca4a3735916549e795dbe5af110e6ced4135df
4rrow <4rrow88@gmail.com>
2016-12-12 09:37:09 +1300

    Add some blank xaml pages for prestart forms

---
+++ PreStart/PreStart/Pages/PrestartForm3.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace PreStart.Pages
+{
+    public partial class PrestartForm3 : ContentPage
+    {
+        public PrestartForm3()
+        {
+            InitializeComponent();
+        }
+    }
+}
