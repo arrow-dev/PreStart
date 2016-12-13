1c56848c49e3cc79bf5368a8f9f4993d798d4217
4rrow <4rrow88@gmail.com>
2016-12-13 16:21:33 +1300

    Added Models

---
+++ PreStart/PreStart/Models/Hazard.cs
@@ -0,0 +1,19 @@
+﻿using PreStart.Abstractions;
+
+namespace PreStart.Models
+{
+    public class Hazard : TableData
+    {
+        public string Description { get; set; }
+
+        public string Repercussion { get; set; }
+
+        public int RiskBefore { get; set; }
+
+        public string Response { get; set; }
+
+        public int RiskAfter { get; set; }
+
+        public string TaskId { get; set; }
+    }
+}
