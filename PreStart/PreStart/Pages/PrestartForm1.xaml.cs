b9cc44d112a7512b3319746604916b19f9c8ec17
splashyin <yin.hao@rocketmail.com>
2016-12-14 14:01:37 +1300

    s

--- PreStart/PreStart/Pages/PrestartForm1.xaml.cs
+++ PreStart/PreStart/Pages/PrestartForm1.xaml.cs
@@ -10,7 +10,11 @@ namespace PreStart.Pages
         {
             InitializeComponent();
             BindingContext = new PrestartForm1ViewModel();
+
+
         }
+
+        
     }
 
 
--- PreStart/PreStart/ViewModels/PrestartForm1ViewModel.cs
+++ PreStart/PreStart/ViewModels/PrestartForm1ViewModel.cs
@@ -1,4 +1,11 @@
-﻿namespace PreStart.ViewModels
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PreStart.ViewModels
 {
     class PrestartForm1ViewModel
     {
