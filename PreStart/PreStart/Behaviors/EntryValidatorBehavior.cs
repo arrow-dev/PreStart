d8b8bf6952c82f484aa14907cc41fa274af2563f
4rrow <4rrow88@gmail.com>
2016-12-16 08:36:50 +1300

    Create basic entry validator behavior

---
+++ PreStart/PreStart/Behaviors/EntryValidatorBehavior.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace PreStart.Behaviors
+{
+    public class EntryValidatorBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += Entry_TextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= Entry_TextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var entry = ((Entry) sender);
+            bool isValid = entry.Text.Length > 0;
+            entry.BackgroundColor = isValid ? Color.Default : Color.Red;
+        }
+    }
+}
