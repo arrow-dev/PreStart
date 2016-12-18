3f9e0e66690ff0d02ffd5ae6ce58b4ff7fe66f89
splashyin <yin.hao@rocketmail.com>
2016-12-19 12:02:39 +1300

    19/12anthony

---
+++ PreStart/PreStart/Behaviors/BehaviorsTest.cs
@@ -0,0 +1,44 @@
+﻿
+using Xamarin.Forms;
+
+namespace PreStart.Behaviors
+{
+
+    public class BehaviorsTest : Behavior<Entry>
+    {
+
+
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+
+        }
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+
+        }
+
+        // Behavior implementation
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+
+            string empty = "";
+            bool isValid = true;
+            if (args.ToString().Length != empty.Length & isValid)
+            {
+                ((Entry) sender).BackgroundColor = Color.Red;
+                isValid = false;
+            }
+            else
+            {
+                ((Entry)sender).BackgroundColor = Color.Aqua;
+                isValid = true;
+            }
+
+        }
+    }
+
+}
--- PreStart/PreStart/ViewModels/PrestartForm1ViewModel.cs
+++ PreStart/PreStart/ViewModels/PrestartForm1ViewModel.cs
@@ -3,6 +3,8 @@ using PreStart.Models;
 using PreStart.Pages;
 using System;
 using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
 using Xamarin.Forms;
 using Task = System.Threading.Tasks.Task;
 
@@ -10,11 +12,14 @@ namespace PreStart.ViewModels
 {
     public class PrestartForm1ViewModel : BaseViewModel
     {
+        
         public Prestart Prestart { get; set; }
+        
 
         public PrestartForm1ViewModel()
         {
             Prestart = new Prestart();
+            
         }
 
         Command preFillCommand;
@@ -31,6 +36,25 @@ namespace PreStart.ViewModels
             try
             {
                 //Get yesterday's form and prefill some properties
+                var table = await App.CloudService.GetTableAsync<Prestart>();
+                var items = await table.ReadAllItemsAsync();
+                items.OrderByDescending(I => I.CreatedAt);
+                var item = items.Last();
+                Prestart.Department = item.Department;
+                Prestart.ContractName = item.ContractName;
+                Prestart.ContractNumber = item.ContractNumber;
+                Prestart.Location = item.Location;
+                Prestart.LotNo = item.LotNo;
+                Prestart.Project = item.Project;
+                Prestart.JobNumber = item.JobNumber;
+                Prestart.TmpNumber = item.TmpNumber;
+                Prestart.SiteFirstAider = item.SiteFirstAider;
+                Prestart.CertificateNumber = item.CertificateNumber;
+                Prestart.QuarryManager = item.QuarryManager;
+                Prestart.SiteManager = item.SiteManager;
+                Prestart.StmsNumber = item.StmsNumber;
+                
+
             }
             catch (Exception ex)
             {
@@ -66,5 +90,39 @@ namespace PreStart.ViewModels
                 IsBusy = false;
             }
         }
+
+        private static DateTime dayTime = DateTime.Now;
+
+        private static string fixdate(DateTime a)
+        {
+           
+            string temp = String.Format("{0:d/M/yyyy}", a);
+            return temp;
+        }
+
+        private string dateString = fixdate(dayTime);
+
+        public string DateString
+        {
+            set
+            {
+                if (dateString != value)
+                {
+                    dateString = value;
+                    OnPropertyChanged("DateString");
+                }
+            }
+            get
+            {
+               
+                return dateString;
+                
+            }
+
+            
+        }
+
+
+    
     }
 }
