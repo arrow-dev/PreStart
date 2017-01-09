7d3d7ecdcf9dd7443a40237e7f2f3c9b42ed3890
4rrow <4rrow88@gmail.com>
2017-01-10 11:35:56 +1300

    .

--- PreStart/PreStart/ViewModels/TaskDetailViewModel.cs
+++ PreStart/PreStart/ViewModels/TaskDetailViewModel.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using PreStart.Abstractions;
+﻿using PreStart.Abstractions;
 using PreStart.Models;
-using PreStart.Pages;
-using Xamarin.Forms;
 using PreStart.Pages;
 using System;
 using System.Collections.ObjectModel;
@@ -52,6 +47,10 @@ namespace PreStart.ViewModels
             get { return selectedItem; }
             set
             {
+                if (IsBusy)
+                {
+                    return;
+                }
                 SetProperty(ref selectedItem, value, "SelectedItem");
                 if (selectedItem != null)
                 {
@@ -61,87 +60,6 @@ namespace PreStart.ViewModels
                 }
             }
         }
-        Command newCommand;
-
-        public Command NewCommand
-            => newCommand ?? (newCommand = new Command(async () => await ExecuteNewCommand()));
-
-        async System.Threading.Tasks.Task ExecuteNewCommand()
-        {
-            if (IsBusy)
-                return;
-            IsBusy = true;
-
-            try
-            {
-                Application.Current.MainPage.Navigation.PushAsync(new HazardForm(new Hazard {TaskId = Task.Id}));
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"{ex.Message}");
-            }
-            finally
-            {
-                IsBusy = false;
-            }
-        }
-
-        Command deleteCommand;
-
-        public Command DeleteCommand
-            => deleteCommand ?? (deleteCommand = new Command<string>(async (string id) => await ExecuteDeleteCommand(id)));
-
-        async System.Threading.Tasks.Task ExecuteDeleteCommand(String id)
-        {
-            if (IsBusy)
-                return;
-            IsBusy = true;
-
-            try
-            {
-                var table = await App.CloudService.GetTableAsync<Hazard>();
-                var hazard = await table.ReadItemAsync(id);
-                await table.DeleteItemAsync(hazard);
-                GetHazardsAsync();
-                await App.CloudService.SyncOfflineCacheAsync();
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"{ex.Message}");
-            }
-            finally
-            {
-                IsBusy = false;
-            }
-        }
-    }
-}
-
-
-        private ObservableCollection<Hazard> hazards = new ObservableCollection<Hazard>();
-
-        public ObservableCollection<Hazard> Hazards
-        {
-            get { return hazards; }
-            set { SetProperty(ref hazards, value, "Hazards"); }
-        }
- 
-
-        public async void GetHazardsAsync()
-        {
-            var table = await App.CloudService.GetTableAsync<Hazard>();
-            var items = await table.ReadAllItemsAsync();
-            Hazards.Clear();
-            foreach (var aHazard in items)
-            {
-                if (aHazard.TaskId == Task.Id)
-                {
-                    Hazards.Add(aHazard);
-                }
-            }
-        }
-
-
 
         private Command addHazard;
 
