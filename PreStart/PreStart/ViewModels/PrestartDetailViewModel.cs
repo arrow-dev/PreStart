25edf0e07e11c1e682b9d83a595379c1464c641e
Ryan <Ryant1234@gmail.com>
2017-01-13 14:30:38 +1300

    Got the Triggers for the Images working

---
+++ PreStart/PreStart/ViewModels/PrestartDetailViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PreStart.Models;
+
+namespace PreStart.ViewModels
+{
+  public  class PrestartDetailViewModel
+    {
+        public Prestart Prestart { get; set; }
+
+
+        public PrestartDetailViewModel(Prestart prestart)
+        {
+            Prestart = prestart;
+        }
+    }
+}
