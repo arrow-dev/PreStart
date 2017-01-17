81b3fc16f7ac898cddcbad8ce0f663b32f37be7f
splashyin <yin.hao@rocketmail.com>
2017-01-17 14:05:02 +1300

    searchbar

--- PreStart/PreStart/Pages/SiteListPage.xaml.cs
+++ PreStart/PreStart/Pages/SiteListPage.xaml.cs
@@ -1,14 +1,46 @@
-﻿using PreStart.ViewModels;
+﻿using System;
+using System.Collections.ObjectModel;
+using PreStart.Models;
+using PreStart.ViewModels;
 using Xamarin.Forms;
 
 namespace PreStart.Pages
 {
     public partial class SiteListPage : ContentPage
     {
+        private static SiteListPageViewModel SiteListVM = new SiteListPageViewModel();
+
         public SiteListPage()
         {
             InitializeComponent();
             BindingContext = new SiteListPageViewModel();
+            
         }
+
+        void OnSearchBarTextChanged(object sender, TextChangedEventArgs args)
+        {
+            resultsStack.Children.Clear();
+        }
+
+        void OnSearchBarButtonPressed(object sender, EventArgs args)
+        {
+            resultsScroll.Content = null;
+            resultsStack.Children.Clear();
+            SearchSite(SiteListVM.Sites);
+            resultsScroll.Content = resultsStack;
+        }
+
+        void SearchSite(ObservableCollection<Location> Sites)
+        {
+            foreach (var item in Sites)
+            {
+                if (item.Name == SiteSearchBar.Text)
+                {
+                    
+                }
+            }
+            
+        }
+
     }
 }
