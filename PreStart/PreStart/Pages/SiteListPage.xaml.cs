53c9faf3dd9b3b52e77db0cfe22995a37b7bd32b
splashyin <yin.hao@rocketmail.com>
2017-01-17 14:48:43 +1300

    anthony

--- PreStart/PreStart/Pages/SiteListPage.xaml.cs
+++ PreStart/PreStart/Pages/SiteListPage.xaml.cs
@@ -8,39 +8,13 @@ namespace PreStart.Pages
 {
     public partial class SiteListPage : ContentPage
     {
-        private static SiteListPageViewModel SiteListVM = new SiteListPageViewModel();
-
+       
         public SiteListPage()
         {
             InitializeComponent();
             BindingContext = new SiteListPageViewModel();
             
         }
-
-        void OnSearchBarTextChanged(object sender, TextChangedEventArgs args)
-        {
-            resultsStack.Children.Clear();
-        }
-
-        void OnSearchBarButtonPressed(object sender, EventArgs args)
-        {
-            resultsScroll.Content = null;
-            resultsStack.Children.Clear();
-            SearchSite(SiteListVM.Sites);
-            resultsScroll.Content = resultsStack;
-        }
-
-        void SearchSite(ObservableCollection<Location> Sites)
-        {
-            foreach (var item in Sites)
-            {
-                if (item.Name == SiteSearchBar.Text)
-                {
-                    
-                }
-            }
-            
-        }
-
+        
     }
 }
