90e51e38601d96575ddbbc6821174f443c4e9f3f
4rrow <4rrow88@gmail.com>
2017-01-31 09:22:54 +1300

    Add windows 8 project

---
+++ PreStart/PreStart.Windows/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PreStart.Windows
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new PreStart.App());
+        }
+    }
+}
