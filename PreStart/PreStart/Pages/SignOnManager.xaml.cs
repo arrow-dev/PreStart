dad740906c430f9a1d61c975391c0b617cf73369
4rrow <4rrow88@gmail.com>
2017-01-30 15:05:23 +1300

    Add Sign In manager

---
+++ PreStart/PreStart/Pages/SignOnManager.xaml.cs
@@ -0,0 +1,14 @@
+﻿using PreStart.ViewModels;
+using Xamarin.Forms;
+
+namespace PreStart.Pages
+{
+    public partial class SignOnManager : ContentPage
+    {
+        public SignOnManager(string id)
+        {
+            InitializeComponent();
+            BindingContext = new SignOnManagerViewModel(id, Navigation);
+        }
+    }
+}
