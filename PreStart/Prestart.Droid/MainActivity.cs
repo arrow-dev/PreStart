4b034eefe031a3fc033c7d8a63c13c1c97cde548
4rrow <4rrow88@gmail.com>
2017-02-20 10:53:38 +1300

    Temp fix for Android bug

--- PreStart/Prestart.Droid/MainActivity.cs
+++ PreStart/Prestart.Droid/MainActivity.cs
@@ -1,11 +1,14 @@
 ﻿
+
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using System;
 
 namespace Prestart.Droid
 {
-    [Activity(Label = "Prestart", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Prestart", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true,
+         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
@@ -17,6 +20,19 @@ namespace Prestart.Droid
             Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+        }
+
+        //TODO Remove when bug is fixed.
+        protected override void OnDestroy()
+        {
+            try
+            {
+                base.OnDestroy();
+            }
+            catch (Exception)
+            {
+                
+            }
         }
     }
 }
