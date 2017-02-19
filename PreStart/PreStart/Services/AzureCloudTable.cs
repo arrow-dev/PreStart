914566c512a0ffc318555c7b4e7c204bee6e1b05
4rrow <4rrow88@gmail.com>
2017-02-20 11:19:45 +1300

    Data objects Id field's are now set locally to avoid issues if a prestart is created when device is offline

--- PreStart/PreStart/Services/AzureCloudTable.cs
+++ PreStart/PreStart/Services/AzureCloudTable.cs
@@ -19,6 +19,7 @@ namespace Prestart.Services
         
         public async Task<T> CreateItemAsync(T item)
         {
+            item.Id = new Guid().ToString();
             item.DateCreated = DateTime.Now;
             await table.InsertAsync(item);
             return item;
