3e87d3f0e9b31b8fd4bb08f2104c403a3ec64823
yinhao <yinhao@DESKTOP-JCB6H11>
2017-02-24 10:22:14 +1300

    a

--- PreStart/PreStart/Model/Hazard.cs
+++ PreStart/PreStart/Model/Hazard.cs
@@ -24,7 +24,7 @@ namespace Prestart.Model
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
